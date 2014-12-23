a3900fb1503c4927343517cc157eb2c2a74ebaef
Shadow97175 <Shadow_97@mail.ru>
2014-12-24 00:02:36 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,83 +1,88 @@
 Programming
 ===========
-/*
-	Есть матрица 5 x 5, нужно сделать так, что бы значение каждой ячейки заменялось
-	на среднее арифметическое этой ячейки и всех ячеек вокруг. При этом каждая следующая ячейка 
-	принимает во внимание изменения предыдущей.
-*/
-
-#include <iostream>
-#define N 5
-using namespace std;
-
-int main(void)
-{
-	setlocale(0, "Russian");
-	int A[N][N];
-	cout<<"Введите матрицу A 5x5: "<<endl;
-	for (int i=0; i<N; i++)
-	{
-		for (int j=0; j<N; j++)
-		{
-			cin>>A[i][j];
-		}
-	}
-	for (int i=0; i<N; i++)
-	{
-		for (int j=0, summ=0, count=1; j<N; j++)
-		{
-			if (i-1>0 && j-1>0)
-			{
-				summ+=A[i-1][j-1];
-				count++;
-			}
-			if (i-1>0)
-			{
-				summ+=A[i-1][j];
-				count++;
-			}
-			if (i+1<N && j-1>0)
-			{
-				summ+=A[i+1][j-1];
-				count++;
-			}
-			if (j-1>0)
-			{
-				summ+=A[i][j-1];
-				count++;
-			}
-			if (j+1<N)
-			{
-				summ+=A[i][j+1];
-				count++;
-			}
-			if (i+1<N && j-1>0)
-			{
-				summ+=A[i+1][j-1];
-				count++;
-			}
-			if (i+1<N)
-			{
-				summ+=A[i+1][j];
-				count++;
-			}
-			if (i+1<N && j+1<N)
-			{
-				summ+=A[i+1][j+1];
-				count++;
-			}
-			summ+=A[i][j];
-			A[i][j]=summ/count;
-		}
-	}
-	cout<<"Матрица после преобразования: "<<endl;
-	for (int i=0; i<N; i++)
-	{
-		for (int j=0; j<N; j++)
-		{
-			cout<<A[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
-}
+    /*
+            Есть матрица 5 x 5, нужно сделать так, что бы значение каждой ячейки заменялось на среднее арифметическое этой ячейки и всех ячеек вокруг.
+            При этом, каждая следующая ячейка не принимает во внимание изменения предыдущих
+    */
+     
+    #include <iostream>
+    #define N 5
+    using namespace std;
+     
+    int main()
+    {
+            setlocale(0, "Russian");
+            int A[N][N], safeA[N][N];
+            cout<<"Введите матрицу A 5x5: "<<endl;
+            for (int i=0; i<N; i++)
+            {
+                    for (int j=0; j<N; j++)
+                    {
+                            cin>>A[i][j];
+                            safeA[i][j]=A[i][j];
+                    }
+            }
+     
+            for (int i=0; i<N; i++)
+            {
+                    for (int j=0, summ=0, count=1; j<N; j++)
+                    {
+                            //      верхняя строка
+                            if (i-1>0 && j-1>0)
+                            {
+                                    summ+=safeA[i-1][j-1];
+                                    count++;
+                            }
+                            if (i-1>0)
+                            {
+                                    summ+=safeA[i-1][j];
+                                    count++;
+                            }
+                            if (i-1>0 && j+1<N)
+                            {
+                                    summ+=safeA[i-1][j+1];
+                                    count++;
+                            }
+                            //      слева
+                            if (j-1 > 0)
+                            {
+                                    summ+=safeA[i][j-1];
+                                    count++;
+                            }
+                            //      справа
+                            if (j+1<N)
+                            {
+                                    summ+=safeA[i][j+1];
+                                    count++;
+                            }
+                            //      нижняя строка
+                            if (i+1<N && j-1>0)
+                            {
+                                    summ+=safeA[i+1][j-1];
+                                    count++;
+                            }
+                            if (i+1<N)
+                            {
+                                    summ+=safeA[i+1][j];
+                                    count++;
+                            }
+                            if (i+1<N && j+1<N)
+                            {
+                                    summ+=safeA[i+1][j+1];
+                                    count++;
+                            }
+     
+                            summ+=safeA[i][j];
+                            A[i][j]=summ/count;
+                    }
+            }
+     
+            cout<<"Матрица после преобразования: "<<endl;
+            for (int i=0; i<N; i++)
+            {
+                    for (int j=0; j<N; j++)
+                            cout<<A[i][j]<<" ";
+                    cout<<endl;
+            }
+            return 0;
+    }

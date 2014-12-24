13121028cc045572fcd1ae966a7465e26d49357d
Shadow97175 <Shadow_97@mail.ru>
2014-12-25 01:30:34 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -10,18 +10,18 @@ int main(void)
 	setlocale(0, "Russian");
 	char s[128];
 	int count[((RUS_CHARS*2)+(ENG_CHARS*2))]={0};
-	cout<<"Ââåäèòå ñòðîêó: "<<endl;
+	cout<<"Введите строку: "<<endl;
 	cin>>s;
 	for (int i=0; i<strlen(s); i++)
 	{	
-		if (s[i]>='À' && s[i]<='ÿ')			count[(('z'-'A')+1+(s[i]-'À'))]++;	//	ðóññ áóêâû
-		else if (s[i]>='A' && s[i]<='z')	count[(s[i]-'A')]++;						//	àíãë áóêâû
+		if (s[i]>='À' && s[i]<='ÿ')			count[(('z'-'A')+1+(s[i]-'À'))]++;	
+		else if (s[i]>='A' && s[i]<='z')	count[(s[i]-'A')]++;						
 	}
 	char a;
-	cout<<"Ââåäèòå áóêâó êîëè÷åñòâî âõîæäåíèé êîòîðîé Âû õîòèòå óçíàòü: "<<endl;
+	cout<<"Введите букву, количество вхождений которой Вы хотите узнать: "<<endl;
 	cin>>a;
-	if (a>='À' && a<='ÿ')		cout<<"Êîëè÷åñòâî âõîæäåíèé: "<<count[(('z'-'A')+1+(a-'À'))]<<endl;	//	ðóññ áóêâà
-	else if (a>='A' && a<='z')	cout<<"Êîëè÷åñòâî âõîæäåíèé: "<<count[a-'A']<<endl;	//	àíãë áóêâà	
-	else						cout<<"Âû ââåëè íå áóêâó!"<<endl;
+	if (a>='À' && a<='ÿ')		cout<<"Количество вхождений: "<<count[(('z'-'A')+1+(a-'À'))]<<endl;	
+	else if (a>='A' && a<='z')	cout<<"Количество вхождений: "<<count[a-'A']<<endl;	
+	else				cout<<"Вы ввели не букву!"<<endl;
 	return 0;
 }

8a1d03d339cfdfb00aea5aa2ff89738c84fc4127
Shadow97175 <Shadow_97@mail.ru>
2014-12-27 20:38:50 +0300

    Create 5(line.h).h

---
+++ 5(line.h).h
@@ -0,0 +1,28 @@
+Programming
+===========
+#include "stdafx.h"
+
+class Line {
+	friend std::ostream &operator<< (std::ostream &os, const Line &);
+
+public:
+	Line(int, int, int, int, int, int, int, int);
+	Line(const Line&);
+	~Line();
+
+	const Line &operator= (const Line &);
+
+	bool isset();
+	bool SetXYZ(int, int, int);
+
+	bool Set(int, int, int, int, int, int, int, int);
+
+	bool intersection(const Line&);
+	bool coplanar(const Line&);
+	bool pointOnLine(int, int, int);
+
+private:
+	int a1, b1, c1, d1, a2, b2, c2, d2;
+	int x, y, z;
+	bool exist;
+};

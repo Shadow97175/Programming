c17890e20e0caf657a1ceb0c2e38db39b3ef7e52
Shadow97175 <Shadow_97@mail.ru>
2014-12-27 20:52:48 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -12,31 +12,31 @@ int main()
 
 	// Первая прямая
 	int a1, b1, c1, d1, a2, b2, c2, d2;
-	cout << "Ââåäèòå a1, b1, c1, d1, a2, b2, c2, d2 äëÿ ïåðâîé ïðÿìîé: " << endl;
+	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для первой прямой: " << endl;
 	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 	Line line1(a1, b1, c1, d1, a2, b2, c2, d2);
 	while (line1.isset() == false)
 	{
-		cout << "Ïðÿìàÿ íå ñîçäàíà, ïîâòîðèòå ââîä: " << endl;
+		cout << "Прямая не создана, повторите ввод: " << endl;
 		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 		line1.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
-	cout << "Ââåäèòå X, Y, Z äëÿ ïåðâîé ïðÿìîé: " << endl;
+	cout << "Введите X, Y, Z для первой прямой: " << endl;
 	int x, y, z;
 	cin >> x >> y >> z;
 	line1.SetXYZ(x, y, z);
 
 	// Вторая прямая
-	cout << "Ââåäèòå a1, b1, c1, d1, a2, b2, c2, d2 äëÿ âòîðîé ïðÿìîé: " << endl;
+	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: " << endl;
 	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 	Line line2(a1, b1, c1, d1, a2, b2, c2, d2);
 	while (line2.isset() == false)
 	{
-		cout << "Ïðÿìàÿ íå ñîçäàíà, ïîâòîðèòå ââîä: " << endl;
+		cout << "Прямая не создана, повторите ввод: " << endl;
 		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 		line2.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
-	cout << "Ââåäèòå X, Y, Z äëÿ âòîðîé ïðÿìîé: " << endl;
+	cout << "Введите X, Y, Z для второй прямой: " << endl;
 	cin >> x >> y >> z;
 	line2.SetXYZ(x, y, z);
 

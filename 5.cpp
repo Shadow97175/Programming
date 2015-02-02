8c944791ac77a656abe72efcf37ebce983367c2b
Shadow97175 <Shadow_97@mail.ru>
2015-02-02 11:10:22 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -21,10 +21,6 @@ int main()
 		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 		line1.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
-	cout << "Введите X, Y, Z для первой прямой: " << endl;
-	int x, y, z;
-	cin >> x >> y >> z;
-	line1.SetXYZ(x, y, z);
 
 	// Вторая прямая
 	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: " << endl;
@@ -36,9 +32,6 @@ int main()
 		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
 		line2.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
-	cout << "Введите X, Y, Z для второй прямой: " << endl;
-	cin >> x >> y >> z;
-	line2.SetXYZ(x, y, z);
 
 	cout << "Ïðÿìàÿ 1 è 2 " << (line1.intersection(line2) ? "пересекаются" : "не пересекаются") << endl;
 	cout << "Ïðÿìàÿ 1 è 2 " << (line1.intersection(line2) ? "лежат" : "не лежат") << " в одной плоскости" << endl;

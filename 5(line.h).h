1cd4f8c6d8ecb508812400ff4ea0654c5e2d0ab3
Shadow97175 <Shadow_97@mail.ru>
2015-02-03 21:44:42 +0300

    Update 5(line.h).h

--- 5(line.h).h
+++ 5(line.h).h
@@ -4,21 +4,23 @@ class Line {
 	friend std::ostream &operator<< (std::ostream &os, const Line &);
 
 public:
-	Line(int);
+	Line(int, int, int, int, int, int, int, int);
 	Line(const Line&);
 	~Line();
 
 	const Line &operator= (const Line &);
 
 	bool isset();
+	bool SetXYZ(int);
 
 	bool Set(int);
 
 	bool intersection(const Line&);
 	bool coplanar(const Line&);
-	bool pointOnLine(int, int, int);
+	bool pointOnLine(int);
 
 private:
 	int a1, b1, c1, d1, a2, b2, c2, d2;
+	int x, y, z;
 	bool exist;
 };

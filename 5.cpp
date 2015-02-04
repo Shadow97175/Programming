2d0c10607f937e8aba4cf0692cc539e0a37bc8e5
Shadow97175 <Shadow_97@mail.ru>
2015-02-04 23:51:18 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "russian");
+	setlocale(LC_ALL, "Russian");
 
 	// Первая прямая
-	int a1, b1, c1, d1, a2, b2, c2, d2;
+	int a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, a4, b4, c4, d4;
 	cout<<"Введите a1, b1, c1, d1, a2, b2, c2, d2 для первой прямой: "<<endl;
 	cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
 	Line line1(a1, b1, c1, d1, a2, b2, c2, d2);
@@ -26,14 +26,14 @@ int main()
 	line1.SetXYZ(x1, y1, z1);
 
 	// Вторая прямая
-	cout<<"Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: "<<endl;
-	cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
-	Line line2(a1, b1, c1, d1, a2, b2, c2, d2);
+	cout<<"Введите a3, b3, c3, d3, a4, b4, c4, d4 для второй прямой: "<<endl;
+	cin>>a3>>b3>>c3>>d3>>a4>>b4>>c4>>d4;
+	Line line2(a3, b3, c3, d3, a4, b4, c4, d4);
 	while (line2.isset()==false)
 	{
 		cout<<"Прямая не создана, повторите ввод: "<<endl;
-		cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
-		line2.Set(a1, b1, c1, d1, a2, b2, c2, d2);
+		cin>>a3>>b3>>c3>>d3>>a4>>b4>>c4>>d4;
+		line2.Set(a3, b3, c3, d3, a4, b4, c4, d4);
 	}
 
 	cout<<"Прямые 1 и 2"<<(line1.intersection(line2) ? "пересекаются" : "не пересекаются")<<endl;

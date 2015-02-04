2831ae63596e553caae0bf313b8ca54543d13a46
Shadow97175 <Shadow_97@mail.ru>
2015-02-04 22:31:39 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -11,36 +11,36 @@ int main()
 
 	// Первая прямая
 	int a1, b1, c1, d1, a2, b2, c2, d2;
-	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для первой прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
+	cout<<"Введите a1, b1, c1, d1, a2, b2, c2, d2 для первой прямой: "<<endl;
+	cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
 	Line line1(a1, b1, c1, d1, a2, b2, c2, d2);
-	while (line1.isset() == false)
+	while (line1.isset()==false)
 	{
-		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
+		cout<<"Прямая не создана, повторите ввод: "<<endl;
+		cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
 		line1.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
-	cout « "Введите X, Y, Z для первой прямой: " « endl;
-	int x, y, z;
-	cin » x » y » z;
-	line1.SetXYZ(x, y, z);
+	cout<<"Введите X, Y, Z для первой прямой: "<<endl;
+	int x1, y1, z1;
+	cin>>x1>>y1>>z1;
+	line1.SetXYZ(x1, y1, z1);
 
 	// Вторая прямая
-	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
+	cout<<"Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: "<<endl;
+	cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
 	Line line2(a1, b1, c1, d1, a2, b2, c2, d2);
-	while (line2.isset() == false)
+	while (line2.isset()==false)
 	{
-		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
+		cout<<"Прямая не создана, повторите ввод: "<<endl;
+		cin>>a1>>b1>>c1>>d1>>a2>>b2>>c2>>d2;
 		line2.Set(a1, b1, c1, d1, a2, b2, c2, d2);
 	}
 
-	cout << "Прямые 1 и 2 " << (line1.intersection(line2) ? "пересекаются" : "не пересекаются") << endl;
-	cout << "Прямые 1 и 2 " << (line1.intersection(line2) ? "лежат" : "не лежат") << " в одной плоскости" << endl;
-	cout << "Точка A(0, 6, 2) " << (line1.pointOnLine(0, 6, 2) ? "лежит" : "не лежит") << " на прямой 1" << endl;
+	cout<<"Прямые 1 и 2"<<(line1.intersection(line2) ? "пересекаются" : "не пересекаются")<<endl;
+	cout<<"Прямые 1 и 2 "<<(line1.intersection(line2) ? "лежат" : "не лежат")<<" в одной плоскости"<<endl;
+	cout<<"Точка A(0, 6, 2)"<<(line1.pointOnLine(0, 6, 2) ? "лежит" : "не лежит")<<" на прямой 1"<<endl;
 
-	cout << "Прямая 1:\n" << line1 << "Прямая 2:\n" << line2 << endl;
+	cout<<"Прямая 1:\n"<<line1<<"Прямая 2:\n"<<line2<<endl;
 
 	system("pause");
 	return 0;

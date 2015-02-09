bc79f667c9df9a571549c0e2f0ba3d0b5afb9090
Shadow97175 <Shadow_97@mail.ru>
2015-02-09 23:55:54 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -10,35 +10,31 @@ int main()
 	setlocale(LC_ALL, "russian");
 
 	//	Первая прямая
-	int a1, b1, c1, d1, a2, b2, c2, d2;
-	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для первой прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
-	Line line1(a1, b1, c1, d1, a2, b2, c2, d2);
+	int a1, b1, c1, m1, n1, p1, a2, b2, c2, m2, n2, p2;
+	cout << "Введите a, b, c, m, n, p для первой прямой: " << endl;
+	cin >> a1 >> b1 >> c1 >> d1 >> m1 >> n1 >> p1;
+	Line line1(a1, b1, c1, m1, n1, p1);
 	while (line1.isset() == false)
 	{
 		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
-		line1.Set(a1, b1, c1, d1, a2, b2, c2, d2);
+		cin >> a1 >> b1 >> c1 >> m1 >> n1 >> p1;
+		line1.Set(a1, b1, c1, m1, n1, p1);
 	}
 	cout << "Введите X, Y, Z для первой прямой: " << endl;
-	int x, y, z;
 	cin >> x >> y >> z;
 	line1.SetXYZ(x, y, z);
 
 	//	Вторая прямая
-	cout << "Введите a1, b1, c1, d1, a2, b2, c2, d2 для второй прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
-	Line line2(a1, b1, c1, d1, a2, b2, c2, d2);
+	cout << "Введите a, b, c, m, n, p для второй прямой: " << endl;
+	cin >> a2 >> b2 >> c2 >> m2 >> n2 >> p2;
+	Line line2(a2, b2, c2, m2, n2, p2);
 	while (line2.isset() == false)
 	{
 		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> d1 >> a2 >> b2 >> c2 >> d2;
-		line2.Set(a1, b1, c1, d1, a2, b2, c2, d2);
+		cin >> a2 >> b2 >> c2 >> m2 >> n2 >> p2;
+		line2.Set(a2, b2, c2, m2, n2, p2);
 	}
-	cout << "Введите X, Y, Z для второй прямой: " << endl;
-	cin >> x >> y >> z;
-	line2.SetXYZ(x, y, z);
-
+	
 	cout << "Прямая 1 и 2 " << (line1.intersection(line2) ? "пересекаются" : "не пересекаются") << endl;
 	cout << "Прямая 1 и 2 " << (line1.intersection(line2) ? "лежат" : "не лежат") << " в одной плоскости" << endl;
 	cout << "Точка A(0, 6, 2) " << (line1.pointOnLine(0, 6, 2) ? "лежит" : "не лежит") << " на прямой 1" << endl;

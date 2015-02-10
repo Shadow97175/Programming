d33f162404b9894e5e80f50a2a546b2215ba864f
Shadow97175 <Shadow_97@mail.ru>
2015-02-10 18:11:58 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "russian");
+	setlocale(LC_ALL, "Russian");
 
 	// Первая прямая
 	int a1, b1, c1, m1, n1, p1;
@@ -18,36 +18,17 @@ int main()
 	cin >> m1 >> n1 >> p1;
 	Line line1(a1, b1, c1, m1, n1, p1);
 	
-	while (line1.isset() == false)
-	{
-		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> m1 >> n1 >> p1;
-		line1.Set(a1, b1, c1, m1, n1, p1);
-	}
-
-	cout << "Введите X, Y, Z для первой прямой: " << endl;
-	cin >> x >> y >> z;
-	line1.SetXYZ(x, y, z);
 
 	// Вторая прямая
-	cout << "Введите a, b, c, m, n, p для второй прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> m1 >> n1 >> p1;
-	Line line2(a2, b2, c2, m2, n2, p2);
-	
-	while (line2.isset() == false)
-	{
-		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a2 >> b2 >> c2 >> m2 >> n2 >> p2;
-		line2.Set(a1, b1, c1, m1, n1, p1);
-	}
-	
-	cout << "Введите X, Y, Z для второй прямой: " << endl;
-	cin >> x >> y >> z;
-	line1.SetXYZ(x, y, z);
+	cout << "Введите координаты точки a, b, c для первой прямой: " << endl;
+	cin >> a1 >> b1 >> c1;
+	cout << "Введите координаты направляющего вектора m, n, p для первой прямой: " << endl;
+	cin >> m1 >> n1 >> p1;
+	Line line2(a1, b1, c1, m1, n1, p1);
 
 	cout << "Прямая 1 и 2 " << (line1.intersection(line2) ? "пересекаются" : "не пересекаются") << endl;
 	cout << "Прямая 1 и 2 " << (line1.coplanar(line2) ? "лежат" : "не лежат") << " в одной плоскости" << endl;
-	cout << "Точка A(0, 6, 2) " << (line1.pointOnLine(0, 6, 2) ? "лежит" : "не лежит") << " на прямой 1" << endl;
+	cout << "Точка A " << (line1.pointOnLine(point) ? "лежит" : "не лежит") << " на прямой 1" << endl;
 	cout << "Прямая 1:\n" << line1 << "Прямая 2:\n" << line2 << endl;
 	
 	return 0;

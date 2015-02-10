b3c4f06d5a7c0f4825160bcf7ee2e990b0583ff6
Shadow97175 <Shadow_97@mail.ru>
2015-02-10 15:16:28 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,7 +1,8 @@
 /*
-Программа должна уметь создавать прямые через коэффициэнты (ax+by+cz+d). Также программа должна 
+Программа должна уметь создавать прямые через коэффициэнты (ax+by+cz+d). Также программа должна
 определять пересекаются ли прямые, находятся ли они в любой плоскости, и лежит ли данная точка на прямой.
 */
+
 #include "stdafx.h"
 using namespace std;
 
@@ -9,37 +10,44 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	//	Первая прямая
+	// Первая прямая
 	int a1, b1, c1, m1, n1, p1;
 	cout << "Введите a, b, c, m, n, p для первой прямой: " << endl;
 	cin >> a1 >> b1 >> c1 >> d1 >> m1 >> n1 >> p1;
 	Line line1(a1, b1, c1, m1, n1, p1);
+	
 	while (line1.isset() == false)
 	{
 		cout << "Прямая не создана, повторите ввод: " << endl;
 		cin >> a1 >> b1 >> c1 >> m1 >> n1 >> p1;
 		line1.Set(a1, b1, c1, m1, n1, p1);
 	}
+
 	cout << "Введите X, Y, Z для первой прямой: " << endl;
 	cin >> x >> y >> z;
 	line1.SetXYZ(x, y, z);
 
-	//	Вторая прямая
+	// Вторая прямая
+	int a2, b2, c2, m2, n2, p2;
 	cout << "Введите a, b, c, m, n, p для второй прямой: " << endl;
-	cin >> a1 >> b1 >> c1 >> d1 >> m1 >> n1 >> p1;
-	Line line2(a1, b1, c1, m1, n1, p1);
+	cin >> a2 >> b2 >> c2 >> m2 >> n2 >> p2;
+	Line line2(a2, b2, c2, m2, n2, p2);
+	
 	while (line2.isset() == false)
 	{
 		cout << "Прямая не создана, повторите ввод: " << endl;
-		cin >> a1 >> b1 >> c1 >> d1 >> m1 >> n1 >> p1;
-		line2.Set(a1, b1, c1, m1, n1, p1);
+		cin >> a2 >> b2 >> c2 >> m2 >> n2 >> p2;
+		line2.Set(a2, b2, c2, m2, n2, p2);
 	}
 	
+	cout << "Введите X, Y, Z для второй прямой: " << endl;
+	cin >> x >> y >> z;
+	line1.SetXYZ(x, y, z);
+
 	cout << "Прямая 1 и 2 " << (line1.intersection(line2) ? "пересекаются" : "не пересекаются") << endl;
 	cout << "Прямая 1 и 2 " << (line1.coplanar(line2) ? "лежат" : "не лежат") << " в одной плоскости" << endl;
 	cout << "Точка A(0, 6, 2) " << (line1.pointOnLine(0, 6, 2) ? "лежит" : "не лежит") << " на прямой 1" << endl;
-
 	cout << "Прямая 1:\n" << line1 << "Прямая 2:\n" << line2 << endl;
-
+	
 	return 0;
 }

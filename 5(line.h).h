ec52e8ab562e82d1fb622fc19cdf2783678bd4ed
Shadow97175 <Shadow_97@mail.ru>
2015-02-10 16:57:33 +0300

    Update 5(line.h).h

--- 5(line.h).h
+++ 5(line.h).h
@@ -4,7 +4,7 @@ class Line {
 	friend std::ostream &operator<< (std::ostream &os, const Line &);
 
 public:
-	Line(int, int, int, int, int, int, int, int, int, int, int, int);
+	Line(int, int, int, int, int, int);
 	Line(const Line&);
 	~Line();
 
@@ -13,7 +13,7 @@ public:
 	bool isset();
 	bool SetXYZ(int, int, int);
 
-	bool Set(int, int, int, int, int, int, int, int, int, int, int, int);
+	bool Set(int, int, int, int, int, int);
 
 	bool intersection(const Line&);
 	bool coplanar(const Line&);
@@ -21,7 +21,6 @@ public:
 
 private:
 	int a1, b1, c1, m1, n1, p1;
-	int a2, b2, c2, m2, n2, p2;
 	int x, y, z;
 	bool exist;
 };

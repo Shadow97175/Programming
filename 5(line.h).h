0dac1e54fb8edf17826ed94e8e17c8a3069feb6a
Shadow97175 <Shadow_97@mail.ru>
2015-02-26 02:27:18 +0300

    Update 5(line.h).h

--- 5(line.h).h
+++ 5(line.h).h
@@ -1,26 +1,26 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 
 class Line {
 	friend std::ostream &operator<< (std::ostream &os, const Line &);
 
 public:
-	Line(int, int, int, int, int, int);
+	Line(int x, int y, int z, int vx, int vy, int vz);
 	Line(const Line&);
 	~Line();
 
 	const Line &operator= (const Line &);
+	
+	bool isset() const;
 
-	bool isset();
-	bool SetXYZ(int, int, int);
-
-	bool Set(int, int, int, int, int, int);
+	bool Set(int x, int y, int z, int vx, int vy, int vz);
 
 	bool intersection(const Line&);
 	bool coplanar(const Line&);
-	bool pointOnLine(int);
+	bool parallel(const Line&);
+	bool pointOnLine(int, int, int);
 
 private:
-	int a1, b1, c1, m1, n1, p1;
-	int x, y, z;
-	bool exist;
+	int X, Y, Z;
+	int Vx, Vy, Vz;
+	bool Initialized;
 };

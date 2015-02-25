e651a2fa5450caa5da2ab74279770f4dddf22da7
Shadow97175 <Shadow_97@mail.ru>
2015-02-26 02:26:50 +0300

    Update 5(line.cpp).cpp

--- 5(line.cpp).cpp
+++ 5(line.cpp).cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include "line.h"
 
-Line::Line(int a1, b1, c1, m1, n1, p1)
+Line::Line(int x, int y, int z, int vx, int vy, int vz)
 {
-	Set(a1, b1, c1, m1, n1, p1);
+	Set(x, y, z, vx, vy, vz);
 }
 
 Line::Line(const Line& l)
 {
-
-	*this = a1=l.a1, b1=l.b1, c1=l.c1, m1=l.m1, n1=l.n1, p1=l.p1;
+	X=l.X;
+	Y=l.Y;
+	Z=l.Z;
+	Vx=l.Vx;
+	Vy=l.Vy;
+	Vz=l.Vz;
 }
 
 Line::~Line()
@@ -16,50 +21,101 @@ Line::~Line()
 
 }
 
-bool Line::Set(int a1, b1, c1, m1, n1, p1)
+bool Line::Set(int x, int y, int z, int vx, int vy, int vz)
 {
-
+	X=x;
+	Y=y;
+	Z=z;
+	Vx=vx;
+	Vy=vy;
+	Vz=vz;
+	Initialized = true;
+	return true;
 }
 
-bool Line::isset()
+bool Line::isset() const
 {
-	return exist;
-}
-
-bool Line::SetXYZ(int x, y, z)
-{
-	if (exist)
-	{
-		this->x = x;	this->y = y;	this->z = z;
-		return true;
-	}
-	return false;
+	return Initialized;
 }
 
 const Line &Line::operator= (const Line &l)
 {
 	if (&l != this)
 	{
-		if (exist)
-		{
-			this->a1 = l.a1; this->b1 = l.b1; this->c1 = l.c1; this->m1 = l.m1; this->n1 = l.n1; this->p1 = l.p1;
-			exist = true;
-		}
+		X=l.X;
+		Y=l.Y;
+		Z=l.Z;
+		Vx=l.Vx;
+		Vy=l.Vy;
+		Vz=l.Vz;
 	}
 	return *this;
 }
 
 bool Line::intersection(const Line& l)
 {
-	return (this.coplanar(l) && (m1*l.n1-n1*l.m1 != 0 || m1*l.p1-p1*l.m1 != 0 || n1*l.p1-p1*l.n1 != 0)) ? true : false;
+	if (Vx == 0)
+	{
+		if (l.Vx == 0)
+		{
+			return X == l.X;
+		}
+		else
+		{
+			int t2 = (X-l.X)/l.Vx;
+			if (Vy == 0)
+			{
+				return Y == l.Y + l.Vy * t2;
+			}
+			else
+			{
+				int t1=(l.Y + l.Vy*t2 - Y)/Vy;
+				return Z+Vz*t1==l.Z+l.Vz*t2;
+			}
+		}
+	}
+	else
+	{
+		int d = l.Vy-Vy*l.Vx/Vx; 
+		if (d==0)
+		{
+			return Y - l.Y + Vy*(l.X - X)/Vx == 0;
+		}
+		else
+		{
+			int t2 = (Y - l.Y + Vy*(l.X - X)/Vx)/d;
+			int t1 = (l.X-X+l.Vx*t2)/Vx;
+			return Z+Vz*t1==l.Z+l.Vz*t2;
+		}
+	}
+}
+
+bool Line::parallel(const Line& l)
+{
+	return (l.Vx*Vy == Vx*l.Vy) && (l.Vx*Vz != Vx*l.Vz) && (l.Vy*Vz != Vy*l.Vz) &&
+			!pointOnLine(l.X,l.Y,l.Z);
 }
 
 bool Line::coplanar(const Line& l)
 {
-	return (((l.a1 - a1) * n1 * l.p1 + (l.b1 - b1) * p1 * l.m1 + (l.c1 - c1) * m1 * l.n1 - (l.c1 - c1) * n1 * l.m1 + (l.a1 - a1) * p1 * l.n1 + (l.b1 - b1) * m1 * l.p1) == 0) ? true : false;
-} 
+	return parallel(l) || intersection(l);
+}
 
-bool Line::pointOnLine(int x, y, z)
+bool Line::pointOnLine(int x, int y, int z)
 {
-	return (((x-a1)*n1 == (y-b1)*m1) && ((y-b1)*p1 == (z-c1)*n1) && ((x-a1)*p1 == (z-c1)*m1)) ? true : false;
+	return ((X - x)*Vy == (Y - y)*Vx) && ((X - x)*Vz == (Z - z)*Vx) && ((Y - y)*Vz == (Z - z)*Vy);
+}
+
+std::ostream &operator<< (std::ostream &os, const Line &l)
+{
+	if (l.isset())
+	{
+		os << "Прямая задана точкой (" << l.X << ", " << l.Y << ", " << l.Z
+		   << ") и вектором (" << l.Vx << ", " << l.Vy << ", " << l.Vz << ")\n";
+	}
+	else
+	{
+		os << "Прямая не задана.\n";
+	}
+	return os;
 }
